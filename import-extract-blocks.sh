db5c476d7ab176a20415fb8eaeb23d84d6ccc4f8
Kamil Jezek <kamil.jezek@gmail.com>
2020-04-27 10:23:32 +1000

    added script for parity nano instrument

--- import-extract-blocks.sh
+++ import-extract-blocks.sh
@@ -4,18 +4,18 @@ HEAP_SIZE=32986
 GETH_EXE=/home/kjezek/go-ethereum/build/bin/geth
 BLOCKS_PATH=/mnt/backup/kirk/blocks
 DB_PATH=/home/kjezek/_geth_db
-ANALYSER_PATH=/home/kjezek/ethereum_storage_analysis
+ANALYSER_PATH=/home/kjezek/ethereum-storage-analysis/
 
 BLOCKS_ARRAY=("0-1M.blockchain" "1-2M.blockchain"  "2-3M.blockchain"  "3-4M.blockchain"  "4-5M.blockchain"  "5-6M.blockchain"  "6-7M.blockchain"  "7-8M.blockchain" "8-9M.blockchain")
 
 process_block_height () {
   local block=$1
-  local DB_PATH="$DB_BLOCKS_PATH/$block/geth/chaindata"
-  echo "Executed analysis for $DB_PATH"
+  local LEVEL_DB_PATH="$DB_PATH/geth/chaindata"
+  echo "Executed analysis for $LEVEL_DB_PATH"
 
-  node --max-old-space-size=$HEAP_SIZE ../blocks-one-latest-main.js "$DB_PATH"
-  node --max-old-space-size=$HEAP_SIZE ../tries-main.js "$DB_PATH"
-  node --max-old-space-size=$HEAP_SIZE ../storage-tries-main.js "$DB_PATH"
+  node --max-old-space-size=$HEAP_SIZE ../blocks-one-latest-main.js "$LEVEL_DB_PATH"
+  node --max-old-space-size=$HEAP_SIZE ../tries-main.js "$LEVEL_DB_PATH"
+  node --max-old-space-size=$HEAP_SIZE ../storage-tries-main.js "$LEVEL_DB_PATH"
 }
 
 #rm -rf $DB_PATH
---
+++ parity-run-nano.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# Run nano instrumentation for Parity.
+# Import 0.1M Sample block into respective block dump
+# Collect data in CSV files
+#
+
+PARITY_PATH=/home/kjezek/parity-ethereum/
+PARITY_PATH_EXE="$PARITY_PATH/target/release/parity"
+LOGS_BACKUP_PATH=/mnt/backup/kamil
+DB_PATH=/home/kjezek/_etherum_data/
+
+JAVA_ANALYSER_EXE="$PARITY_PATH/research-1911/parityloganalyser/target/parity-log-analyser-1.0-SNAPSHOT-spring-boot.jar"
+
+BLOCKS_SAMPLES_ARRAY=("0.1M.blockchain" "1.1M.blockchain" "2.1M.blockchain" "3.1M.blockchain" "4.1M.blockchain" "5.1M.blockchain" "6.1M.blockchain" "7.1M.blockchain" "8.1M.blockchain" "9.1M.blockchain")
+DB_BLOCK_HEIGHTS_DUMPS=( "io.parity.0" "io.parity.1" "io.parity.2" "io.parity.3" "io.parity.4" "io.parity.5" "io.parity.6" "io.parity.7" "io.parity.8" "io.parity.9")
+
+rm *.csv
+
+for i in "${!BLOCKS_SAMPLES_ARRAY[@]}"; do
+
+  # Maybe we need to copy DB dumps from the backup hdd as SSD will run out of space
+
+  CURRENT_DB_PATH="$DB_PATH/${!DB_BLOCK_HEIGHTS_DUMPS[i]}"
+  CURRENT_BLOCK_PATH="$DB_PATH/blocks/${!BLOCKS_SAMPLES_ARRAY[i]}"
+  block="${!BLOCKS_SAMPLES_ARRAY[i]}"
+
+  $PARITY_PATH_EXE import "$CURRENT_BLOCK_PATH" --base-path="$CURRENT_DB_PATH"  --no-warp
+
+  java -jar $JAVA_ANALYSER_EXE .
+  mv aggregated.csv.txt "aggregated.$block.txt"
+  mkdir "$LOGS_BACKUP_PATH/logs.$block"
+  mv *.csv "$LOGS_BACKUP_PATH/logs.$block/"
+  rm *.csv
+
+done
